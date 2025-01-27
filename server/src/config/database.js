6877ed7c41e69542957581216eb9b5c6d6cc59bb
Student <Student@LEC-11-5-PC113.control.ad.sit.kmutt.ac.th>
2025-01-27 17:18:09 +0700

    playlist api

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATBASE,
+    database: process.env.DB_DATABASE,
 })
 
 export default connection;
