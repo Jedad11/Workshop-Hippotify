7c7a2633abc95ace653355779fc59ca73ca23c48
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2025-01-31 15:57:45 +0700

    add get user example

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connection from "./config/database.js";
+import db from "./config/database.js";
 import playlistRoute from "./routes/playlistRoute.js";
 
 const app = express();
@@ -8,6 +9,29 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/api/playlists", playlistRoute);
+
+const userId = 26;
+app.get("/api/user", async (req, res) => {
+    try {
+        const [response] = await db.promise().query(
+            `SELECT id, username, email
+            FROM users
+            WHERE id = ?`, [userId]
+        );
+        return res.status(200).json({
+            success: true,
+            data: response,
+            message: "User retrieved successfully"
+        });
+    } catch (error) {
+        console.log("Error: ",error);
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: "Internal server error"
+        });
+    }
+})
 
 connection.connect((err)=>{
     if(err){
