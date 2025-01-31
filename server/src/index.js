e670a936b8e3cfad6ef5f9848ac9655ee7deb98c
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2025-01-31 16:43:57 +0700

    update sql

--- server/src/index.js
+++ server/src/index.js
@@ -12,35 +12,36 @@ app.use("/api/playlists", playlistRoute);
 
 const userId = 26;
 app.get("/api/user", async (req, res) => {
-    try {
-        const [response] = await db.promise().query(
-            `SELECT id, username, email
-            FROM users
-            WHERE id = ?`, [userId]
-        );
-        return res.status(200).json({
-            success: true,
-            data: response,
-            message: "User retrieved successfully"
-        });
-    } catch (error) {
-        console.log("Error: ",error);
-        return res.status(500).json({
-            success: false,
-            data: null,
-            message: "Internal server error"
-        });
-    }
-})
+  try {
+    const [response] = await db.promise().query(
+      `SELECT id, image_url, email, username, role
+             FROM users 
+             WHERE id = ?`,
+      [userId]
+    );
+    return res.status(200).json({
+      success: true,
+      data: response,
+      message: "User retrieved successfully",
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Internal server error",
+    });
+  }
+});
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Database is connected");
-    }
-})
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database is connected");
+  }
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
