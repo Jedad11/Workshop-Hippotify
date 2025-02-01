90b707488f5d845ed5e09908512749676be85839
Student <Student@LEC-10-3-PC211.control.ad.sit.kmutt.ac.th>
2025-02-01 10:43:16 +0700

    add user resource

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connection from "./config/database.js";
 import playlistRoute from "./routes/playlistRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 const port = 3000;
@@ -8,31 +9,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/api/playlists", playlistRoute);
-
-import db from "./config/database.js";
-const userId = 26;
-app.get("/api/user", async (req, res) => {
-  try {
-    const [response] = await db.promise().query(
-      `SELECT id, image_url, email, username, role
-             FROM users 
-             WHERE id = ?`,
-      [userId]
-    );
-    return res.status(200).json({
-      success: true,
-      data: response,
-      message: "User retrieved successfully",
-    });
-  } catch (error) {
-    console.log("Error: ", error);
-    return res.status(500).json({
-      success: false,
-      data: null,
-      message: "Internal server error",
-    });
-  }
-});
+app.use("/api/users", userRoute);
 
 connection.connect((err) => {
   if (err) {
